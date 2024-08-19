365b354d675b0d4842cb80f331e407356ef50531
Kacper Szczepanski <110172487+Kagbah@users.noreply.github.com>
2024-08-19 21:34:14 +0200

    fix: missing key

--- src/app/protected/page.tsx
+++ src/app/protected/page.tsx
@@ -14,9 +14,8 @@ export default async function ProtectedPage() {
     return redirect("/login");
   }
 
-  const questions = await supabase.from("questions").select("*")
+  const questions = await supabase.from("questions").select("*");
 
-  console.log(questions.data)
   return (
     <div className="flex-1 w-full flex flex-col gap-20 items-center">
       <div className="w-full">
@@ -26,7 +25,6 @@ export default async function ProtectedPage() {
         </div>
         <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
           <div className="w-full max-w-4xl flex justify-between items-center p-3 text-sm">
-            
             <AuthButton />
           </div>
         </nav>
@@ -36,9 +34,9 @@ export default async function ProtectedPage() {
         <Header />
         <main className="flex-1 flex flex-col gap-6">
           <h2 className="font-bold text-4xl mb-4">Next steps</h2>
-            {questions.data?.map((elem) =>{
-                return(<span>{elem.questionText}</span>)
-            })}
+          {questions.data?.map((elem) => {
+            return <span key={elem.id}>{elem.questionText}</span>;
+          })}
         </main>
       </div>
 
