c96e313f09020272c9814e948527f946567c3bf9
Kacper Szczepanski <110172487+Kagbah@users.noreply.github.com>
2024-08-19 21:29:18 +0200

    refactor: v2

---
+++ src/app/page.tsx
@@ -0,0 +1,35 @@
+import AuthButton from "@/components/AuthButton";
+import { createClient } from "@/utils/supabase/server";
+import Header from "@/components/Header";
+
+export default async function Index() {
+
+  return (
+    <div className="flex-1 w-full flex flex-col gap-20 items-center">
+      <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
+        <div className="w-full max-w-4xl flex justify-between items-center p-3 text-sm">
+          
+           <AuthButton />
+        </div>
+      </nav>
+
+      <div className="flex-1 flex flex-col gap-20 max-w-4xl px-3">
+        <Header />
+      </div>
+
+      <footer className="w-full border-t border-t-foreground/10 p-8 flex justify-center text-center text-xs">
+        <p>
+          Powered by{" "}
+          <a
+            href="https://supabase.com/?utm_source=create-next-app&utm_medium=template&utm_term=nextjs"
+            target="_blank"
+            className="font-bold hover:underline"
+            rel="noreferrer"
+          >
+            Supabase
+          </a>
+        </p>
+      </footer>
+    </div>
+  );
+}
