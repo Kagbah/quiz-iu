136299c6bbff2a5b5962e8b03be33d28a320bf67
Kacper Szczepanski <110172487+Kagbah@users.noreply.github.com>
2024-08-21 21:21:22 +0200

    feat: sign-up page

---
+++ src/app/sign-up/page.tsx
@@ -0,0 +1,52 @@
+import { Metadata } from "next";
+import Image from "next/image";
+import Link from "next/link";
+
+import { cn } from "@/lib/utils";
+import { buttonVariants } from "@/components/ui/button";
+import { UserAuthForm } from "@/components/user-auth-form-sign-up";
+import { Logo } from "@/components/Logo";
+
+export const metadata: Metadata = {
+  title: "Authentication",
+  description: "Authentication forms built using the components.",
+};
+
+export default function AuthenticationPage() {
+  return (
+    <>
+      <div
+        className="min-w-full min-h-screen bg-center bg-cover bg-no-repeat"
+        style={{ backgroundImage: "url('/media/bg-img.png')" }}
+      >
+        <div className="container relative hidden h-[800px] flex-col items-center justify-center md:flex lg:px-0">
+          <Logo></Logo>
+
+          <div className="lg:p-8 border border-input rounded-lg bg-background drop-shadow-md">
+            <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
+              <div className="flex flex-col space-y-2 text-center">
+                <h1 className="text-2xl font-semibold tracking-tight">
+                  Registrieren
+                </h1>
+                <p className="text-sm text-muted-foreground">
+                  Registrier dich mit deiner E-Mail-Adresse.
+                </p>
+              </div>
+              <UserAuthForm />
+              <p className="px-8 text-center text-sm text-muted-foreground">
+                Du hast bereits einen Account? Dann kannst du dich{" "}
+                <Link
+                  href="/login"
+                  className="underline underline-offset-4 hover:text-primary"
+                >
+                  einloggen
+                </Link>{" "}
+                und loslegen!
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
---
+++ src/components/user-auth-form-sign-up.tsx
@@ -0,0 +1,72 @@
+"use client";
+
+import * as React from "react";
+import { signUp } from "@/components/user-auth-form-server";
+import { cn } from "@/lib/utils";
+import { Icons } from "@/components/ui/icons";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { SubmitButton } from "@/components/submit-button";
+
+interface UserAuthFormProps extends React.HTMLAttributes<HTMLDivElement> {}
+
+export function UserAuthForm({ className, ...props }: UserAuthFormProps) {
+  const [isLoading, setIsLoading] = React.useState<boolean>(false);
+
+  async function onSubmit(event: React.SyntheticEvent) {
+    event.preventDefault();
+    setIsLoading(true);
+
+    const formData = new FormData(event.currentTarget as HTMLFormElement);
+
+    await signUp(formData);
+
+    setIsLoading(false);
+  }
+
+  return (
+    <div className={cn("grid gap-6", className)} {...props}>
+      <form onSubmit={onSubmit}>
+        <div className="grid gap-2">
+          <div className="grid gap-1">
+            <Label className="sr-only" htmlFor="email">
+              Email
+            </Label>
+            <Input
+              id="email"
+              name="email"
+              placeholder="E-Mail"
+              type="email"
+              autoCapitalize="none"
+              autoComplete="email"
+              autoCorrect="off"
+              disabled={isLoading}
+            />
+          </div>
+          <div className="grid gap-1">
+            <Label className="sr-only" htmlFor="password">
+              Password
+            </Label>
+            <Input
+              id="password"
+              name="password"
+              placeholder="Passwort"
+              type="password"
+              autoCapitalize="none"
+              autoCorrect="off"
+              disabled={isLoading}
+            />
+          </div>
+
+          <SubmitButton formAction={signUp} disabled={isLoading}>
+            {isLoading && (
+              <Icons.spinner className="mr-2 h-4 w-4 animate-spin" />
+            )}
+            Registrieren
+          </SubmitButton>
+        </div>
+      </form>
+    </div>
+  );
+}
