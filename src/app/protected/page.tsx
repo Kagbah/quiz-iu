daddc432ea58229ee55ac1ea8cfc63f3a8ccb022
Kacper Szczepanski <110172487+Kagbah@users.noreply.github.com>
2024-08-21 21:23:46 +0200

    wip: logged in page

--- src/app/protected/page.tsx
+++ src/app/protected/page.tsx
@@ -1,4 +1,4 @@
-export const runtime = 'edge';
+export const runtime = "edge";
 import AuthButton from "@/components/AuthButton";
 import { createClient } from "@/utils/supabase/server";
 import Header from "@/components/Header";
@@ -20,10 +20,6 @@ export default async function ProtectedPage() {
   return (
     <div className="flex-1 w-full flex flex-col gap-20 items-center">
       <div className="w-full">
-        <div className="py-6 font-bold bg-purple-950 text-center">
-          This is a protected page that you can only see as an authenticated
-          user
-        </div>
         <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
           <div className="w-full max-w-4xl flex justify-between items-center p-3 text-sm">
             <AuthButton />
