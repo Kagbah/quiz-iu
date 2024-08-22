c27dadf857c133e021a49432606ec8069153e5f4
Kacper Szczepanski <110172487+Kagbah@users.noreply.github.com>
2024-08-22 15:27:34 +0200

    chore: authbutton redesign

--- src/components/AuthButton.tsx
+++ src/components/AuthButton.tsx
@@ -1,6 +1,7 @@
 import { createClient } from "@/utils/supabase/server";
 import Link from "next/link";
 import { redirect } from "next/navigation";
+import { Logo } from "@/components/Logo";
 
 export default async function AuthButton() {
   const supabase = createClient();
@@ -19,19 +20,22 @@ export default async function AuthButton() {
 
   return user ? (
     <div className="flex items-center justify-between gap-4 w-full">
-      <p>Hey, {user.email}!</p>
+      <Logo></Logo>
       <form action={signOut}>
-        <button className="py-2 px-4 rounded-md no-underline bg-btn-background hover:bg-btn-background-hover">
+        <button className="py-2 px-2 rounded-md no-underline bg-btn-background hover:bg-btn-background-hover">
           Logout
         </button>
       </form>
     </div>
   ) : (
-    <Link
-      href="/login"
-      className="py-2 px-3 flex rounded-md no-underline bg-btn-background hover:bg-btn-background-hover"
-    >
-      Login
-    </Link>
+    <div className="flex items-center justify-between gap-4 w-full">
+      <Logo></Logo>
+      <Link
+        href="/login"
+        className="py-2 px-3 flex rounded-md no-underline bg-btn-background hover:bg-btn-background-hover"
+      >
+        Login
+      </Link>
+    </div>
   );
 }
