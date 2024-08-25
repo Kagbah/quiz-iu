2ba6fec7fb868df4f3d4a9663c04ee3ca9bc70c0
Kacper Szczepanski <110172487+Kagbah@users.noreply.github.com>
2024-08-25 13:10:09 +0200

    wip: help and admin pages

---
+++ src/app/help/page.tsx
@@ -0,0 +1,29 @@
+export const runtime = "edge";
+import AuthButton from "@/components/AuthButton";
+import Header from "@/components/HeaderLanding";
+import Link from "next/link";
+
+export default async function Index() {
+  return (
+    <div className="flex-1 w-full flex flex-col gap-20 items-center">
+      <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
+        <div className="w-full max-w-4xl flex justify-between items-center p-3 text-sm">
+          <AuthButton />
+        </div>
+      </nav>
+
+      <div className="flex-1 flex flex-col gap-20 max-w-4xl px-3">
+        Ich benötige Hilfe!
+      </div>
+
+      <footer className="w-full border-t border-t-foreground/10 p-8 flex justify-center text-center text-xs">
+        <div className="flex justify-evenly text-sm text-muted-foreground gap-8">
+          <Link href="#">Datenschutz</Link>
+          <Link href="#">Impressum</Link>
+          <Link href="/help">Hilfe</Link>
+          <Link href="/admin-login">Login für Admins</Link>
+        </div>
+      </footer>
+    </div>
+  );
+}
