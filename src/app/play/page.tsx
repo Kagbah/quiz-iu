284872a4ff02831030289a606584e8fc1a3d0444
Kacper Szczepanski <110172487+Kagbah@users.noreply.github.com>
2024-08-27 19:16:27 +0200

    feat: playing page

--- src/app/play/page.tsx
+++ src/app/play/page.tsx
@@ -5,7 +5,7 @@ import { createClient } from "@/utils/supabase/server";
 import { redirect } from "next/navigation";
 import Header from "@/components/HeaderLoggedIn";
 import { Footer } from "@/components/Footer";
-import React from 'react';
+import React from "react";
 import LobbySelector from "@/components/LobbySelector";
 import LobbyCreator from "@/components/LobbyCreator";
 
@@ -22,21 +22,11 @@ export default async function ProtectedPage() {
 
   return (
     <div className="flex-1 w-full flex flex-col gap-20 items-center">
-      <div className="w-full">
-        <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
-          <div className="w-full max-w-4xl flex justify-between items-center p-3 text-sm">
-            <AuthButton />
-          </div>
-        </nav>
+      <div className="flex-1 flex flex-col gap-8 max-w-4xl px-3 justify-center">
+        <LobbySelector />
+        <LobbyCreator />
+        <hr />
       </div>
-      <LobbySelector />
-      <hr />
-      <LobbyCreator />
-      <div className="flex-1 flex flex-col gap-20 max-w-4xl px-3">
-        <Header />
-      </div>
-      <Footer />
     </div>
   );
 }
-
