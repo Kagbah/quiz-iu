620be856907d713bc893c186f4a38d3baad19a94
Kagbah <kacper.szczepanski@ruxiom.de>
2024-08-28 09:29:46 +0200

    feat: admin-dashboard page

---
+++ src/app/admin-dashboard/page.tsx
@@ -0,0 +1,67 @@
+"use server";
+
+import { ChartPie, TrendingUp } from "lucide-react";
+import { Area, AreaChart, CartesianGrid, XAxis } from "recharts";
+
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import {
+  ChartConfig,
+  ChartContainer,
+  ChartTooltip,
+  ChartTooltipContent,
+} from "@/components/ui/chart";
+import { createClient } from "@/utils/supabase/server";
+import { redirect } from "next/navigation";
+import DisplayPieChart from "@/components/ChartPie";
+import DisplayCard from "@/components/DashboardCard";
+import DisplayRadialChart from "@/components/ChartRadial";
+const chartData = [
+  { month: "January", desktop: 186, mobile: 80 },
+  { month: "February", desktop: 305, mobile: 200 },
+  { month: "March", desktop: 237, mobile: 120 },
+  { month: "April", desktop: 73, mobile: 190 },
+  { month: "May", desktop: 209, mobile: 130 },
+  { month: "June", desktop: 214, mobile: 140 },
+];
+
+const chartConfig = {
+  desktop: {
+    label: "Desktop",
+    color: "hsl(var(--chart-1))",
+  },
+  mobile: {
+    label: "Mobile",
+    color: "hsl(var(--chart-2))",
+  },
+} satisfies ChartConfig;
+
+export default async function ProtectedPage() {
+  const supabase = createClient();
+
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
+
+  if (!user) {
+    return redirect("/login");
+  }
+
+  const questions = await supabase.from("questions").select("*");
+  return (
+    <div className="flex flex-wrap gap-8 p-8">
+      <DisplayPieChart></DisplayPieChart>
+      <div className="flex flex-col gap-8">
+        <DisplayCard></DisplayCard>
+        <DisplayCard></DisplayCard>
+      </div>
+      <DisplayRadialChart></DisplayRadialChart>
+    </div>
+  );
+}
