4186a1ff5edd7a32ca6873d258e330aa207815e4
Kagbah <kacper.szczepanski@ruxiom.de>
2024-08-28 09:31:01 +0200

    wip: missing pages

---
+++ src/app/settings/page.tsx
@@ -0,0 +1,25 @@
+export const runtime = "edge";
+import { createClient } from "@/utils/supabase/server";
+import { redirect } from "next/navigation";
+import React from "react";
+import CategoryManagement from "@/components/QuizManagement";
+// import './CreateQuiz.css';
+import QuizManagement from "@/components/QuizManagement";
+
+export default async function ProtectedPage() {
+  const supabase = createClient();
+
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
+
+  if (!user) {
+    return redirect("/login");
+  }
+
+  return (
+    <div className="flex-1 w-full flex flex-col gap-20 items-center">
+      settings
+    </div>
+  );
+}
