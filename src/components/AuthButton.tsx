f148319182c9f822d65836a6333a3cf84c763f18
Kagbah <kacper.szczepanski@ruxiom.de>
2024-09-20 09:11:35 +0200

    chore: header button to logout removed

--- src/components/AuthButton.tsx
+++ src/components/AuthButton.tsx
@@ -21,11 +21,6 @@ export default async function AuthButton() {
   return user ? (
     <div className="flex items-center justify-between gap-4 w-full">
       <Logo></Logo>
-      <form action={signOut}>
-        <button className="py-2 px-2 rounded-md no-underline bg-btn-background hover:bg-btn-background-hover">
-          Logout
-        </button>
-      </form>
     </div>
   ) : (
     <div className="flex items-center justify-between gap-4 w-full">
