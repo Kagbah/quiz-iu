f23dbbc184f14b14155df15cb18b15d32f687d23
Kagbah <kacper.szczepanski@ruxiom.de>
2024-09-20 09:24:58 +0200

    chore: redirection to right url

--- src/actions/leave-lobby.ts
+++ src/actions/leave-lobby.ts
@@ -6,5 +6,5 @@ import { redirect } from "next/navigation";
 export async function leaveLobby(userId: string) {
   const supabase = createClient();
   await supabase.from("lobbies_user").delete().eq("user_id", userId);
-  redirect("/play");
+  redirect("/app/play");
 }
